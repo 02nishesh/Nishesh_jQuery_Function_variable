d0645a559e759baf5576a0da6cb66c30e8b581fd
Graphic <graphic@SOD-SJH-605-15M.local>
2016-02-02 16:23:56 -0500

    First Ex done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+$('.switch').on('click', function () {
+  $('body').toggleClass('dark');
 
+      if ($('button').hasClass('on') ) {
+          $('button').removeClass('on');
+          $('button').addClass('off');
+          $('.status').text('Hey, who turn off the lights');
+  }
+      else {
+        $('button').removeClass('off');
+        $('button').addClass('on');
+        $('.status').text('Its so bright in here');
 
+      }
+
+});
